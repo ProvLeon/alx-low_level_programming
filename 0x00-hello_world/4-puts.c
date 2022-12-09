2662092780c1c648f5d84a80d63b356a54b2098f
ProvLeon <providence.leonard@gmail.com>
2022-12-08 21:09:58 -0800

    This is my first C programme

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/* This will return a string */
+/* This will return a string 
+ * main is the entry point
+ * if no error occurs, main returns nothing
+ */
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
