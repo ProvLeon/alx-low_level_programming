44e66273a213fbf1e7cda55f802fdd1f7ed322ef
ProvLeon <providence.leonard@gmail.com>
2022-12-08 21:23:52 -0800

    This prints some string of words

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
-/* 
+/*
+ * main - entry point
  * This is printed out using the printf function 
- * main is the entry point of the programme
  * the main function returns no value if no error occurs
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
