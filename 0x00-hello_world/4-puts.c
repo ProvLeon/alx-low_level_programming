2785c6cfed4766b487aa7f33dfb25a926ef2e938
ProvLeon <providence.leonard@gmail.com>
2022-12-08 21:17:17 -0800

    These prints some string of words out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/* This will return a string 
+/* 
+ * This will return a string 
  * main is the entry point
  * if no error occurs, main returns nothing
  */
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/* This is printed out using the printf function 
+/* 
+ * This is printed out using the printf function 
  * main is the entry point of the programme
  * the main function returns no value if no error occurs
  */
 
-int main (void)
+int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
