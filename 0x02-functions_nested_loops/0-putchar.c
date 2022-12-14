ed2d9a403a915b42f0b5a6778291ec8e4e0867f9
ProvLeon <providence.leonard@gmail.com>
2022-12-13 17:07:45 -0800

    firts commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main: prints _putchar
+ * main - Prints "_putchar" and followed by a new line.
  *
- * Return: 0 if sucessful
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
